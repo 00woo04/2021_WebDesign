f3ef9e6489b294b9871facf8d050956a0accb781
00woo04 <2020193@sungil-i.kr>
2021-03-30 20:47:29 +0900

    20210330

---
+++ 210330_exam(basic)/script/script2.js
@@ -0,0 +1,18 @@
+$(".ul > li").mouseover(function(){
+    $(this).find(".submenu").stop().slideDowm(200);
+});
+$(".ul > li").mouseout(function(){
+    $(this).find(".submenu").stop().slideUp(200);
+});
+
+var currentIndex = 0;
+setInterval(function(){
+    if(currentIndex < 2){
+        currentIndex++;
+    }else{
+        currentIndex = 0;
+    }
+    var slidePosition = currentIndex*(-360)+"px";
+
+    $(".slideList").animate({top:slidePosition},400);
+},3000);
